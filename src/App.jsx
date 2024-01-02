e9dfd92f3ba2a0216c0410207ee4c93878fd29cb
Quena <quenallatafranco@gmail.com>
2024-01-02 15:31:20 -0300

    mejorando el about

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import { useState } from 'react'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Portfolio from './Components/Portfolio/Portfolio';
-import ContactMe from './Components/ContactMe/ContactMe';
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import ContactMe from "./Components/ContactMe/ContactMe";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Skills from "./Components/Skills/Skills";
-import ButtonBack from "./Components/ButtonBack/ButtonBack"
-import './App.css'
+import ButtonBack from "./Components/ButtonBack/ButtonBack";
 
 function App() {
- 
-
   return (
     <div>
-       <Header/>
-       <Hero/>
-       <About/>
-       <Skills />
-       <Portfolio/>
-       <ContactMe/>
-       <Footer />
-       <ButtonBack />
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Portfolio />
+      <ContactMe />
+      <Footer />
+      <ButtonBack />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 
 import Image from "../../assets/About.png";
-import { navigation } from "../../data";
-import { Link } from "react-scroll";
+import bg from "../../assets/bg.jpg";
 
 function About() {
+  const mainContainer = {
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover",
+    padding: "20px",
+  };
+
   return (
-    <section className="section bg-secondary" id="about">
+    <section style={mainContainer} id="about">
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-24">
-          <img
-            className="object-cover h-full w-[250px] md:mx-auto lg:mx-0 rounded-2xl"
-            src={Image}
-            alt="img_about"
-          />
+        <div className="flex flex-col xl:flex-row xl:gap-24 gap-7">
+          <div className="flex flex-col items-center justify-center">
+            <img
+              className="object-cover max-w-full max-h-full mx-auto shadow-xl mb-2"
+              src={Image}
+              alt="img_about"
+              style={{
+                boxShadow: "5px -5px 10px rgba(0, 0, 0, 0.15)",
+              }}
+            />
+            <p>hola</p>
+          </div>
+
           <div className="flex flex-col items-ceter text-center lg:items-start lg:text-left">
             <div className="flex flex-col">
-              <h2 className="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block">
-                Franco Quenallata
+              <h2
+                className="mb-4 text-proxima-nova-600 text-accent text-4xl"
+                style={{
+                  fontFamily: "proxima-nova, sans-serif",
+                  fontWeight: 590,
+                  color: "rgb(5 150 105)",
+                  fontSize: "2rem",
+                }}
+              >
+                About
               </h2>
-              <p className="mb-4 text-emerald-600 text-xl">
-                Full Stack Developer
-              </p>
-              <hr className="mb-8 opacy-5" />
-              <p className="mb-8">
+              <hr className="mb-2 border-t border-gray-500 border-1" />
+              <p className="mb-6">
                 👨🏽‍💻 Full Stack Developer with the ability to create and optimize
                 websites and applications aimed at the end user in a scalable
                 and modularized way. <br />
